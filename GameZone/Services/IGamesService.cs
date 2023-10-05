0a1a5547ff9181268303118bad9882538528aff2
youssef mohamed <yosifmohamed979@hotmail.com>
2023-10-05 14:03:26 +0300

    Add project files.

---
+++ GameZone/Services/IGamesService.cs
@@ -0,0 +1,13 @@
+﻿using GameZone.ViewModel;
+
+namespace GameZone.Services
+{
+    public interface IGamesService
+    {
+        IEnumerable<Game> GetAll();
+        Game? GetById(int id);
+        Task Create(CreateGameFormViewModel model);
+        Task<Game?> Update(EditGameFormViewModel model);
+        bool Delete(int id);
+    }
+}
